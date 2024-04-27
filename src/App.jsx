da25884b4fe400c0fd8e122dfad80eafb9057aa0
邝利豪 <10530797+xianianmian@user.noreply.gitee.com>
2024-04-27 21:02:35 +0800

    0427重复上一次，试试冲突

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ const App = () => {
       <div>
         <p>sdfsfsdf</p>
         这是dev,我现在尝试推送到dev2分支上，刚刚失败了一次
-        <div>又尝试冲突</div>
+        <div>又
+          <p>sdfsdf</p>
+          突</div>
         <div>在世一次</div>
          </div>
       <div>尝试取消提交</div>
